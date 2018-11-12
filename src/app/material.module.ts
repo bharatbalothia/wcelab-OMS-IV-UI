a0387558871aab3750756461238be8a9c35d56a3
Ned Zhang <ned_zhang@us.ibm.com>
2018-11-12 12:52:40 +0800

    Test: add new supply row

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,6 +15,7 @@ import {
     MatTabsModule,
     MatExpansionModule,
     MatAutocompleteModule,
+    MatDatepickerModule,
   } from '@angular/material';
   
   @NgModule({
@@ -32,6 +33,7 @@ import {
       MatTabsModule,
       MatExpansionModule,
       MatAutocompleteModule,
+      MatDatepickerModule,
     ],
     exports: [
       MatSidenavModule,
@@ -47,6 +49,7 @@ import {
       MatTabsModule,
       MatExpansionModule,
       MatAutocompleteModule,
+      MatDatepickerModule,
     ]
   })
 
