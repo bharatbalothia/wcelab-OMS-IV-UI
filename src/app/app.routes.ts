0f47524fa17358b8d114581740cc054b36971291
Ned Zhang <ned_zhang@us.ibm.com>
2018-11-14 11:12:28 +0900

    New Feature: prompt user to login when not authorized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import {SupplyComponent} from './supply/supply.component';
 import {SettingComponent} from './setting/setting.component';
 
 const routes: Routes = [
-  {path: '', component: WelcomeComponent},
+  {path: '/', component: WelcomeComponent},
   // {path: 'shipnode', component: ShipnodeComponent},
   // {path: 'distgroup', component: DistgroupComponent},
   {path: 'supply', component: SupplyComponent},
