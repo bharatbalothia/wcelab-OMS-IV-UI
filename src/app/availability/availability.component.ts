0729efaa4a30c42627bb70b55f6a10139db55a76
Ned Zhang <ned_zhang@us.ibm.com>
2018-11-16 16:11:04 -0600

    New Feature: Complete request for Network and Node availability

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -4,7 +4,7 @@ import {Observable, BehaviorSubject} from 'rxjs';
 
 import { NetworkAvailabilityDataService } from './network-availability-data.service';
 import { NodeAvailabilityDataService } from './node-availability-data.service';
-import { AvaiabilityInquiry, AvailabilityResult } from './availability-data.service';
+import { AvaiabilityInquiry } from './availability-data.service';
 
 
 
@@ -18,17 +18,7 @@ export class AvailabilityComponent implements OnInit {
   // inquiry: AvaiabilityInquiry;
 
   constructor(private networkAvaDataService: NetworkAvailabilityDataService,
-    private nodeAvaDataService: NodeAvailabilityDataService) { 
-    // this.inquiry = {
-    //   distributionGroupId: null,
-    //   segment: null,
-    //   segmentType: null,
-    //   lines: [{
-    //     lineId: 1,
-    //     itemId: '',
-    //     shipNodes: []
-    //   },],
-    // }
+    private nodeAvaDataService: NodeAvailabilityDataService) {
   }
 
   ngOnInit() {
@@ -38,9 +28,44 @@ export class AvailabilityComponent implements OnInit {
     
     console.debug('Query Availability for: ', inquiry);
 
-    let avaResult: Observable<AvailabilityResult> = this.networkAvaDataService.getNetworkAvailability(inquiry);
+    let avaResult = this.getAvailability(inquiry);
 
     avaResult.subscribe(data=>{console.debug(`Received avaiability result: ${JSON.stringify(data)}`)});
   }
+
+  private setInquiryShipnode(inquiry: AvaiabilityInquiry, shipnodeId: string) {
+
+    if (shipnodeId != null && shipnodeId.length > 0){
+      // Search node Availaiblity
+      delete inquiry.distributionGroupId;
+      for (let avaLine of inquiry.lines) {
+        avaLine.shipNodes = [shipnodeId];
+      }
+    } else {
+      // Search network Availability
+      delete inquiry.shipnodeId;
+      for (let avaLine of inquiry.lines) {
+        delete avaLine.shipNodes;
+      }
+    }
+  }
   
+  private getAvailability(inquiry: AvaiabilityInquiry) {
+    
+    if (inquiry.distributionGroupId != null && inquiry.distributionGroupId.length > 0 && inquiry.distributionGroupId != ' ') {
+      
+      console.debug("Getting Networking Availability");
+      this.setInquiryShipnode(inquiry, null);
+      return this.nodeAvaDataService.getNodeAvailability(inquiry);
+
+    } else if (inquiry.shipnodeId != null && inquiry.shipnodeId.length > 0 && inquiry.shipnodeId != ' ') {
+      
+      console.debug("Getting Node Availability");
+      this.setInquiryShipnode(inquiry, inquiry.shipnodeId);
+      return this.networkAvaDataService.getNetworkAvailability(inquiry);
+    }
+
+    
+
+  }
 }
--- src/app/availability/node-availability-data.service.ts
+++ src/app/availability/node-availability-data.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { HttpErrorHandler } from '../http-error-handler.service';
+
+import { IvServiceBase } from '../iv-service-base.service';
+import { IvConstant } from '../iv-constant';
+import { EntityUrl } from '../entity-url';
+import { IVCredent, CredentialDataService } from '../credential/credential-data.service';
+import { AvaiabilityInquiry, ShipnodeAvailability } from './availability-data.service';
+
 @Injectable({
   providedIn: 'root'
 })
-export class NodeAvailabilityDataService {
+export class NodeAvailabilityDataService extends IvServiceBase {
 
-  constructor() { }
+  constructor(http: HttpClient, httpErrorHandler: HttpErrorHandler, credentialData: CredentialDataService) {
+    super(http, httpErrorHandler, credentialData);
+   }
+
+  
+  public getBearerToken = (credential: IVCredent)  => {
+    return credential == null ? null : credential.tokens.availabilityNode; 
+  }
+
+  getEntityUrl(): string {
+    return EntityUrl.AVAILABILITY_NODE;
+  }
+
+  getNodeAvailability(inquiry: AvaiabilityInquiry): Observable<ShipnodeAvailability> {
+    return this.postObject<AvaiabilityInquiry>(inquiry);
+  }
 }
